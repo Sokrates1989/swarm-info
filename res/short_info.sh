bc2e7b2a6080b619d882e45212e2e861735cf0ea
Patrick Michiels <it@patrickmichiels.de>
2024-07-17 17:16:22 +0200

    Attempting to improve service readability

--- res/short_info.sh
+++ res/short_info.sh
@@ -26,7 +26,7 @@ print_info() {
         printf "%-${output_tab_space}s" "$text"
         shift 2
         if [ "$#" -gt 0 ]; then
-            printf " | "
+            printf "  | "
         fi
     done
     echo ""  # Print a newline at the end
@@ -82,7 +82,7 @@ done
 
 echo "List of Nodes in the Swarm:"
 for node in $(docker node ls --format '{{.ID}}'); do
-    node_name=$(docker node ls --filter id=$node --format 'Name: {{.Hostname}}')
+    node_name=$(docker node ls --filter id=$node --format '{{.Hostname}}')
     node_id=$(docker node ls --filter id=$node --format 'ID: {{.ID}}')
     node_status=$(docker node ls --filter id=$node --format 'Status: {{.Status}}')
     node_avail=$(docker node ls --filter id=$node --format 'Availability: {{.Availability}}')
@@ -102,26 +102,55 @@ echo "Detailed Node Information:"
 
 
 # List of Services.
-# Count chars of longest service name.
-max_length=0
-for service in $(docker service ls --format '{{.ID}}'); do
+
+# Count chars of longest values.
+max_name_length=0
+max_id_length=0
+max_mode_length=0
+max_replicas_length=0
+max_image_length=0
+for node in $(docker node ls --format '{{.ID}}'); do
+
+    # Get individual values to print out later.
     service_name=$(docker service ls --filter id=$service --format '{{.Name}}')
+    service_id=$(docker service ls --filter id=$service --format 'ID: {{.ID}}')
+    service_mode=$(docker service ls --filter id=$service --format 'Mode: {{.Mode}}')
+    service_replicas=$(docker service ls --filter id=$service --format 'Replicas: {{.Replicas}}')
+    service_image=$(docker service ls --filter id=$service --format 'Image: {{.Image}}')
     
-    # Calculate the length of the service name.
+    # Calculate the length of these values.
     name_length=${#service_name}
+    id_length=${#service_id}
+    mode_length=${#service_mode}
+    replicas_length=${#service_replicas}
+    image_length=${#service_image}
     
-    # Update the maximum length and longest name if necessary
-    if (( name_length > max_length )); then
-        max_length=$name_length
+    # Update the maximum lengths if necessary.
+    if (( name_length > max_name_length )); then
+        max_name_length=$name_length
+    fi
+    if (( id_length > max_id_length )); then
+        max_id_length=$id_length
+    fi
+    if (( mode_length > max_mode_length )); then
+        max_mode_length=$mode_length
+    fi
+    if (( replicas_length > max_replicas_length )); then
+        max_replicas_length=$replicas_length
+    fi
+    if (( image_length > max_image_length )); then
+        max_image_length=$image_length
     fi
 done
-output_tab_space=$((max_length + 1))
 
 echo "List of Services:"
 for service in $(docker service ls --format '{{.ID}}'); do
     service_name=$(docker service ls --filter id=$service --format '{{.Name}}')
-    service_info=$(docker service ls --filter id=$service --format 'ID: {{.ID}} | Mode: {{.Mode}} | Replicas: {{.Replicas}} | Image: {{.Image}}')
-    print_info "$service_name" "$service_info"
+    service_id=$(docker service ls --filter id=$service --format 'ID: {{.ID}}')
+    service_mode=$(docker service ls --filter id=$service --format 'Mode: {{.Mode}}')
+    service_replicas=$(docker service ls --filter id=$service --format 'Replicas: {{.Replicas}}')
+    service_image=$(docker service ls --filter id=$service --format 'Image: {{.Image}}')
+    print_info "$service_name" "$max_name_length" "$service_id" "$max_id_length" "$service_mode" "$max_mode_length" "$service_replicas" "$max_replicas_length" "$service_image" "$max_image_length" 
 done
 echo
 

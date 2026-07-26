6af01a151c0d372fe5a0c538fbee334bde45601d
Patrick Michiels <it@patrickmichiels.de>
2026-07-27 00:01:38 +0200

    [Functionality] Basic swarm-info script: added tool status checks and update recommendations

--- res/basic_swarm_info.sh
+++ res/basic_swarm_info.sh
@@ -50,6 +50,79 @@ while getopts ":fwmt:c:" opt; do
 done
 
 
+# ==========================================================================
+# This tool's state (swarm-info update check).
+# ==========================================================================
+current_dir=$(pwd)
+cd "$MAIN_DIR" 2>/dev/null || true
+
+echo
+echo "-------------------------------------------------------------------------"
+echo "swarm-info tool status"
+echo "-------------------------------------------------------------------------"
+echo -e "Fetching state of swarm-info (this tool) ..."
+repo_url=https://github.com/Sokrates1989/swarm-info.git
+is_healthy=true
+repo_issue=false
+local_changes=false
+available_updates=false
+
+# Check remote connection.
+if git ls-remote --exit-code "$repo_url" >/dev/null 2>&1; then
+
+    # Check local changes.
+    if [ -n "$(git status --porcelain)" ]; then
+        local_changes=true
+        is_healthy=false
+    fi
+
+    # Check for upstream changes.
+    git fetch -q
+    behind_count=$(git rev-list HEAD..origin/main --count)
+    if [ "$behind_count" -gt 0 ]; then
+        available_updates=true
+        is_healthy=false
+    fi
+else
+    is_healthy=false
+    repo_issue=true
+fi
+
+
+if [ "$is_healthy" = true ]; then
+    echo -e "✅ This tool (swarm-info) is healthy and up to date"
+else
+    # Print detailed information.
+    echo -e "⚠️  This tool (swarm-info) is NOT healthy:"
+
+    if [ "$repo_issue" = true ]; then
+        echo -e "  Remote repository $repo_url is not accessible"
+    else
+        if [ "$local_changes" = true ]; then
+            echo -e "  Local repo has uncommitted changes"
+        fi
+
+        if [ "$available_updates" = true ]; then
+            echo -e "  Remote Repo updateable! $behind_count commits behind. Pull is recommended."
+
+            # Print user info how to update repo.
+            echo -e "\n  To update this tool do this:"
+            echo -e "  cd $MAIN_DIR"
+            echo -e "  git pull"
+            echo -e "  cd $current_dir\n"
+        fi
+    fi
+fi
+echo
+
+# Revert back to the original directory.
+cd "$current_dir" 2>/dev/null || true
+
+
+# ==========================================================================
+# Basic Swarm information.
+# ==========================================================================
+
 # Swarm Status
 echo
 echo "-------------------------------------------------------------------------"
